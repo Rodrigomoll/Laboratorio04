290a196b5a77b3e914b03dda2e044773a7c85ed6
Rodrigo Mollocondo Mendoza <victor.mollocondo@tecsup.edu.pe>
2022-04-12 01:57:38 -0500

    bahoui

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+
+var path = __dirname + '/views/';
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+router.use(function(req, res, next) {
+    console.log("/" + req.method);
+    next();
+});
+
+router.get("/", function(req, res) {
+    res.sendFile(path + "index.html");
+});
+
+router.get("/clientes", function(req, res) {
+    res.sendFile(path + "clientes.html");
+});
+
+router.get("/productos", function(req, res) {
+    res.sendFile(path + "productos.html");
+});
+
+app.use(express.static(path));
+app.use("/", router);
+
+app.listen(8080, function() {
+    console.log('Example app listening on port 8080!')
+})
